24460f42953748dcfba39ee3b96143c3cc3618a5
keller-johnson <keller.s.johnson@gmail.com>
2023-01-26 15:30:54 -0800

    updated bios and jumbotron header to reflects hunters full name

--- script.js
+++ script.js
@@ -24,21 +24,21 @@ function toggleNav() {
   }
 }
 
-function connected() {
-  connectDT.classList.toggle("wallet");
-  if (connectDT.classList.contains("wallet")) {
-    connectDT.innerHTML = "connected";
-  } else {
-    connectDT.innerHTML = "connect wallet";
-  }
+// function connected() {
+//   connectDT.classList.toggle("wallet");
+//   if (connectDT.classList.contains("wallet")) {
+//     connectDT.innerHTML = "connected";
+//   } else {
+//     connectDT.innerHTML = "connect wallet";
+//   }
 
-  connect.classList.toggle("wallet");
-  if (connect.classList.contains("wallet")) {
-    connect.innerHTML = "connected";
-  } else {
-    connect.innerHTML = "connect wallet";
-  }
-}
+//   connect.classList.toggle("wallet");
+//   if (connect.classList.contains("wallet")) {
+//     connect.innerHTML = "connected";
+//   } else {
+//     connect.innerHTML = "connect wallet";
+//   }
+// }
 
 // if wallet is connected change connect wallet to connected
 
